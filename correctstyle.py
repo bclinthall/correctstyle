a9d8c21f2333fc5e09a53bc32d267dcce861ea8c
Clint Hall <hallbc@appstate.edu>
2017-06-10 12:48:39 -0400

    modify to comply with GPL3 and add usage info

--- correctstyle.py
+++ correctstyle.py
@@ -1,9 +1,44 @@
 #! /usr/bin/python2.7
 
+# Run correctstyle.py from command line to offer corrections for common style inconsistencies in C family languages (C, C++, Java, etc)
+# 
+# Copyright (C) 2017  B. Clint Hall
+#
+# B. Clint Hall
+# contact by creating a github issue  https://github.com/bclinthall/correctstyle/
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import re
 import sys
 import os
+
+usage = "usage: correctstyle.py file ..."
+warranty = '''
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
+conditions = '''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version. see <http://www.gnu.org/licenses/>.
+'''
 
 correctstyleDirName = '.correctstyle'
 patterns = [
@@ -26,8 +61,7 @@ patterns = [
     ('\*=([^ ])',                   r'*= \1'),
     ('/([^/*=\n ])',               r'/ \1'),
     ('=([^= ])',                   r'= \1'),
-    ('(?<!-)>([^=>\n ])',          r'> \1'),
-    ('(?<!#include )<([^=< ])',    r'< \1'),
+    ('(?<!-)>([^=>\n ])',          r'> \1'), ('(?<!#include )<([^=< ])',    r'< \1'),
     ('\bif\(',                     r'if ('),
     ('\bfor\(',                    r'for ('),
     ('\bwhile\(',                  r'while ('),
@@ -81,6 +115,18 @@ def getContext(txt, match):
 def splice(txt, insert, start, end):
     return txt[:start] + insert + txt[end:]
 
+def promptForReplacement(fileName, lineNo, pattern, replacement, contextBefore, contextAfter):
+    print   "\n<<<<<<<<<< %s: %d     '%s'  :  '%s'" % (fileName, lineNo, pattern.pattern, replacement)
+    print "%s\n==========\n%s" % (contextBefore, contextAfter)
+    doIt = raw_input(">>>>>>>>>> make this replacement? [yes] or no:  ")
+    if doIt == 'show w':
+        print warranty
+        return promptForReplacement(fileName, lineNo, pattern, replacement, contextBefore, contextAfter)
+    elif doIt == 'show c':
+        print conditions
+        return promptForReplacement(fileName, lineNo, pattern, replacement, contextBefore, contextAfter)
+    else:
+        return len(doIt) == 0 or doIt[0] != 'n' and doIt[0] != 'N'
 
 def correctByPattern(fileName, txt, pattern, replacement):
     pos = 0
@@ -94,10 +140,7 @@ def correctByPattern(fileName, txt, pattern, replacement):
         contextAfter = splice(contextBefore, after, contextMatch.start(), contextMatch.end())
         if not isIgnored(fileName, contextBefore, contextAfter):
             lineNo = txt.count("\n", 0, match.start()) + 1
-            print   "\n<<<<<<<<<< %s: %d     '%s'  :  '%s'" % (fileName, lineNo, pattern.pattern, replacement)
-            print "%s\n==========\n%s" % (contextBefore, contextAfter)
-            doIt = raw_input(">>>>>>>>>> make this replacement? [yes] or no:  ")
-            doIt = len(doIt) == 0 or doIt[0] != 'n' and doIt[0] != 'N'
+            doIt = promptForReplacement(fileName, lineNo, pattern, replacement, contextBefore, contextAfter)
             if doIt:
                 print "replacement made"
                 txt = splice(txt, after, match.start(), match.end())
@@ -123,6 +166,20 @@ def correctFile(fileName):
     
     print "correctstyle finished with " + fileName
 
+print "correctstyle Copyright (C) 2017 B. Clint Hall"
+print "This program comes with ABSOLUTELY NO WARRANTY; for details call `correctstyle.py -w'."
+print "This is free software, and you are welcome to redistribute it under certain conditions; call `correctstyle.py -c' for details."
+
+if len(sys.argv) == 1:
+    print usage
 for fileName in sys.argv[1:]:
-    correctFile(fileName)
+    if fileName[0] == '-':
+        if fileName[1] == 'w':
+            print warranty
+        elif fileName[1] == 'c':
+            print conditions
+        else:
+            print usage
+    else:
+        correctFile(fileName)
 
